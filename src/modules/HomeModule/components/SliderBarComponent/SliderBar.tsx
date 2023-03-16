304efff711a7600ee04649fbd0b83065b5f19b49
artur893 <arthal893@gmail.com>
2023-03-16 23:02:32 +0100

    Swiper nav buttons

--- src/modules/HomeModule/components/SliderBarComponent/SliderBar.tsx
+++ src/modules/HomeModule/components/SliderBarComponent/SliderBar.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom"
 import arrowIcon from '../../../../assets/images/arrow-down.svg'
 import './SliderBar.scss'
 import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 
 interface SliderBarProps {
     movieIcons: string[];
@@ -16,12 +18,17 @@ export const SliderBar: React.FC<SliderBarProps> = ({ movieIcons, category }) =>
         <section className='slider-bar'>
             <div className='slider-bar__info'>
                 <h2 className='slider-bar__category'>{category}</h2>
-                <span className='slider-bar__see-more'>Zobacz wszystkie <img src={arrowIcon} alt="arrow" className='slider-bar__arrow' /></span>
+                <Link to='#'><span className='slider-bar__see-more'>Zobacz wszystkie <img src={arrowIcon} alt="arrow" className='slider-bar__arrow' /></span></Link>
             </div>
             <Swiper
                 slidesPerView={'auto'}
                 spaceBetween={8}
                 loop={true}
+                modules={[Navigation]}
+                navigation={{
+                    nextEl: '.next',
+                    prevEl: '.prev',
+                }}
                 className='slider-bar__swiper'
             >
                 {movieIcons.map((icon, i) => (
@@ -30,6 +37,8 @@ export const SliderBar: React.FC<SliderBarProps> = ({ movieIcons, category }) =>
                 {movieIcons.map((icon, i) => (
                     <SwiperSlide key={i} style={{ width: '256px' }} className='slider-bar__slide'><img src={icon} className='slider-bar__image' /></SwiperSlide>
                 ))}
+                <div className="slider-bar__navigation next"><img src={arrowIcon} /></div>
+                <div className="slider-bar__navigation prev"><img src={arrowIcon} /></div>
             </Swiper>
         </section >
     )
