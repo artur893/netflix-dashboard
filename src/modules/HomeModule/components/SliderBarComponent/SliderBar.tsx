1be4ad50b95eb78480764349134d68c52f8d4ca5
artur893 <arthal893@gmail.com>
2023-03-18 20:25:36 +0100

    Top movies sliders

--- src/modules/HomeModule/components/SliderBarComponent/SliderBar.tsx
+++ src/modules/HomeModule/components/SliderBarComponent/SliderBar.tsx
@@ -9,21 +9,25 @@ import { Navigation } from "swiper";
 
 interface SliderBarProps {
     movieIcons: string[];
-    category: string
+    category: string,
+    width: string,
+    height: string,
+    showPosition: boolean,
+    loop: boolean
 }
 
-export const SliderBar: React.FC<SliderBarProps> = ({ movieIcons, category }) => {
+export const SliderBar: React.FC<SliderBarProps> = ({ movieIcons, category, width, height, showPosition, loop }) => {
 
     return (
         <section className='slider-bar'>
             <div className='slider-bar__info'>
                 <h2 className='slider-bar__category'>{category}</h2>
-                <Link to='#'><span className='slider-bar__see-more'>Zobacz wszystkie <img src={arrowIcon} alt="arrow" className='slider-bar__arrow' /></span></Link>
+                {showPosition || <Link to='#'><span className='slider-bar__see-more'>Zobacz wszystkie <img src={arrowIcon} alt="arrow" className='slider-bar__arrow' /></span></Link>}
             </div>
             <Swiper
                 slidesPerView={'auto'}
                 spaceBetween={8}
-                loop={true}
+                loop={loop}
                 modules={[Navigation]}
                 navigation={{
                     nextEl: '.next',
@@ -31,12 +35,21 @@ export const SliderBar: React.FC<SliderBarProps> = ({ movieIcons, category }) =>
                 }}
                 className='slider-bar__swiper'
             >
-                {movieIcons.map((icon, i) => (
-                    <SwiperSlide key={i} style={{ width: '256px' }} className='slider-bar__slide'><img src={icon} className='slider-bar__image' /></SwiperSlide>
-                ))}
-                {movieIcons.map((icon, i) => (
-                    <SwiperSlide key={i} style={{ width: '256px' }} className='slider-bar__slide'><img src={icon} className='slider-bar__image' /></SwiperSlide>
-                ))}
+                {movieIcons.map((icon, i) => {
+                    if (!showPosition) {
+                        return (
+                            <SwiperSlide key={i} style={{ width: width, height: height }} className='slider-bar__slide'><img src={icon} className='slider-bar__image' /></SwiperSlide>
+                        )
+                    } else {
+                        return (
+                            <SwiperSlide key={i} style={{ width: width, height: height }} className='slider-bar__slide'>
+                                <div className="slider-bar__position">{i + 1}</div>
+                                <img src={icon} style={{ margin: '0px 45px 0px 0px' }} className='slider-bar__image' />
+                            </SwiperSlide>
+                        )
+                    }
+
+                })}
                 <div className="slider-bar__navigation next"><img src={arrowIcon} /></div>
                 <div className="slider-bar__navigation prev"><img src={arrowIcon} /></div>
             </Swiper>
--- src/modules/HomeModule/pages/HomePage/HomePage.tsx
+++ src/modules/HomeModule/pages/HomePage/HomePage.tsx
@@ -7,6 +7,12 @@ import Movie3 from '../../../../assets/images/Movie3.png'
 import Movie4 from '../../../../assets/images/Movie4.png'
 import Movie5 from '../../../../assets/images/Movie5.png'
 import Movie6 from '../../../../assets/images/Movie6.png'
+import Top1 from '../../../../assets/images/Top1.png'
+import Top2 from '../../../../assets/images/Top2.png'
+import Top3 from '../../../../assets/images/Top3.png'
+import Top4 from '../../../../assets/images/Top4.png'
+import Top5 from '../../../../assets/images/Top5.png'
+import Top6 from '../../../../assets/images/Top6.png'
 import { Header } from '../../components/HeaderComponent/Header'
 import { MovieDetails } from '../../components/MovieDetailsComponent/MovieDetails'
 import { SliderBar } from '../../components/SliderBarComponent/SliderBar'
@@ -17,7 +23,11 @@ interface IProps {
 
 export const HomePage: React.FC<IProps> = ({ msg }) => {
 
-  const movieIcons = [Movie1, Movie2, Movie3, Movie4, Movie5, Movie6]
+  const movieIcons = [Movie1, Movie2, Movie3, Movie4, Movie5, Movie6, Movie1, Movie2, Movie3, Movie4, Movie5, Movie6]
+  const movieIcons2 = [Movie4, Movie3, Movie6, Movie5, Movie2, Movie1, Movie4, Movie3, Movie6, Movie5, Movie2, Movie1]
+  const movieIcons3 = [Movie4, Movie5, Movie1, Movie2, Movie3, Movie6, Movie4, Movie5, Movie1, Movie2, Movie3, Movie6]
+  const topIcons = [Top1, Top2, Top3, Top4, Top5, Top6, Top2, Top1, Top3, Top4]
+  const topIcons2 = [Top3, Top4, Top2, Top1, Top5, Top6, Top3, Top4, Top2, Top1]
 
   return (
     <div className="home-page">
@@ -25,9 +35,11 @@ export const HomePage: React.FC<IProps> = ({ msg }) => {
         <img className="home-page__main-img" src={mainImg} alt="wednesday movie" />
         <Header />
         <MovieDetails />
-        <SliderBar movieIcons={movieIcons} category='Docenione przez krytyków' />
-        <SliderBar movieIcons={movieIcons} category='Popularne teraz' />
-        <SliderBar movieIcons={movieIcons} category='Obejrzyj ponownie' />
+        <SliderBar movieIcons={movieIcons} category='Docenione przez krytyków' width='256px' height='164px' showPosition={false} loop={true} />
+        <SliderBar movieIcons={movieIcons2} category='Popularne teraz' width='256px' height='164px' showPosition={false} loop={true} />
+        <SliderBar movieIcons={movieIcons3} category='Obejrzyj ponownie' width='256px' height='164px' showPosition={false} loop={true} />
+        <SliderBar movieIcons={topIcons} category='Top 10 seriali w Polsce dzisiaj' width='auto' height='224px' showPosition={true} loop={false} />
+        <SliderBar movieIcons={topIcons2} category='Top 10 seriali w Polsce dzisiaj' width='auto' height='224px' showPosition={true} loop={false} />
       </div>
     </div>
   );
